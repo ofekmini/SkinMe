76bdd7417f4a068d15351909f6897b2ed6ec863a
rupb902022 <rupb902022@gmail.com>
2022-06-03 11:12:57 +0300

    ap

--- src/screens/CosmeticHomePage.jsx
+++ src/screens/CosmeticHomePage.jsx
@@ -16,7 +16,7 @@ import { Link } from 'react-router-dom';
   btnGetwaitingclients = () => {
     console.clear();
     
-    const  apiUrl= 'https://localhost:44326/api/Cos/GetDepending';
+    const  apiUrl= 'http://localhost:58031/api/Cos/GetDepending';
     fetch(apiUrl, {
       method: 'GET',
       headers: new Headers({
@@ -42,19 +42,57 @@ import { Link } from 'react-router-dom';
           console.log("err post=", error);
         })
       }
+
+      
+
+      btnGetMyclients = () => { 
+        console.clear();
+        
+        const  apiUrl= 'http://localhost:58031/api/Cos/MyClients';
+
+        const cosmetologist_id={
+          cosmetologist_id:this.state.cosmetologist_id
+        };
+        fetch(apiUrl + {
+          method: 'POST',
+          body: JSON.stringify(cosmetologist_id),
+          headers: new Headers({
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json; charset=UTF-8',
+          })
+        })
+          .then(res => {
+            console.log('res=', res);
+            console.log('res.status', res.status);
+            console.log('res.ok', res.ok);
+            return res.json()
+          })
+          .then(
+            (result) => {
+              console.log("fetch btnFetchGetClients= ", result);
+              result.map(st => console.log(st.user_id));
+              console.log('result[0].user_id=', result[0].user_id);
+              this.setState({ users: [...result]}
+                );
+            },
+            (error) => {
+              console.log("err post=", error);
+            })
+          }
+
+
   render() {
     return (
       <div >
         <Logo></Logo>
            <ButtonLogIn  style={{margin:30,backgroundColor:"#c4a092",color:"white",fontSize:15,width:'80%',height:40,borderColor:"#e8e8e8" , borderWidth:1,borderRadius:50}} name="משתמשים ממתינים לחוות דעת " onClick={this.btnGetwaitingclients}/>
-           <ButtonLogIn  style={{margin:30,backgroundColor:"#c4a092",color:"white",fontSize:15,width:'80%',height:40,borderColor:"#e8e8e8" , borderWidth:1,borderRadius:50}} name="לקוחות שלי   " />
+           <ButtonLogIn  style={{margin:30,backgroundColor:"#c4a092",color:"white",fontSize:15,width:'80%',height:40,borderColor:"#e8e8e8" , borderWidth:1,borderRadius:50}} name="לקוחות שלי   " onClick={this.btnGetMyclients} />
            <Link to='/addskinplan'>
            <ButtonLogIn  style={{margin:30,backgroundColor:"#c4a092",color:"white",fontSize:15,width:'80%',height:40,borderColor:"#e8e8e8" , borderWidth:1,borderRadius:50}} name=" יצירת תוכנית טיפוח חדשה   " />
            </Link>
            <Link to='/addproduct' >
            <ButtonLogIn  style={{margin:30,backgroundColor:"#c4a092",color:"white",fontSize:15,width:'80%',height:40,borderColor:"#e8e8e8" , borderWidth:1,borderRadius:50}} name="הוספת מוצר טיפוח  " />
            </Link>
-
       <div >
        {this.state.users.map((users) => <UserCard key={users.user_id} users={users}/>)}
       </div>
