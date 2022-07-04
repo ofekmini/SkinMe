9c6ac953c5b9c464ca3e20b65b6ec18d5e83f060
rupb902022 <rupb902022@gmail.com>
2022-07-04 18:15:45 +0300

    Update AddSkinPlan.jsx

--- src/screens/AddSkinPlan.jsx
+++ src/screens/AddSkinPlan.jsx
@@ -4,32 +4,31 @@ import ButtonLogIn from '../commons/ButtonLogIn';
 import CardAddProdToPlan from '../commons/CardAddProdToPlan';
 import Logo from '../commons/Logo';
 import UserInfo from '../commons/UserInfo';
-import UserCard from '../commons/UserCard';
 import PopUpCos from '../commons/PopUpCos';
 
 
 
 let local = false;
 //const apiUrl = 'https://proj.ruppin.ac.il/bgroup90/test2/tar1/api/';
-    const apiUrl='https://localhost:58031/api/cos';
+const apiUrl = 'https://localhost:58031/api/cos';
 //if (local) {
- // apiUrl = 'https://localhost:44326/api/LogIn/register';
+// apiUrl = 'https://localhost:44326/api/LogIn/register';
 //}
 
- class AddSkinPlan extends Component {
+class AddSkinPlan extends Component {
 
   constructor(props) {
     super(props)
-    
-    this.state={
-      plan_name:"",
-      notes:"",
-      products:[],
-      appUser_id:localStorage.getItem('appUser_id'),
-      users:[],
-           user_status:"waiting",
 
-   }
+    this.state = {
+      plan_name: "",
+      notes: "",
+      products: [],
+      appUser_id: localStorage.getItem('appUser_id'),
+      users: [],
+      user_status: "waiting",
+
+    }
   }
 
   handlechange = (e) => {
@@ -38,9 +37,9 @@ let local = false;
     })
   }
 
-   componentDidMount(){
-  
-    const  apiUrl= 'http://localhost:58031/api/Products/status';
+  componentDidMount() {
+
+    const apiUrl = 'http://localhost:58031/api/Products/status';
 
     fetch(apiUrl, {
       method: 'GET',
@@ -50,7 +49,7 @@ let local = false;
       })
     })
       .then(res => {
-        
+
         console.log('res=', res);
         console.log('res.status', res.status);
         console.log('res.ok', res.ok);
@@ -58,32 +57,31 @@ let local = false;
       })
       .then(
         (result) => {
-          
+
           console.log("fetch btnFetchGetProducts= ", result);
           result.map(st => console.log(st.prod_id));
           console.log('result[0].prod_id', result[0].prod_id);
-          this.setState({ products: [...result]}
-            
-            );
-         
+          this.setState({ products: [...result] }
+
+          );
+
         },
         (error) => {
           console.log("err post=", error);
         })
 
-      } 
+  }
 
- 
 
-  addSkinPlan = (e)=>{
+  addSkinPlan = (e) => {
     console.clear();
     e.preventDefault()
-   
-    const newskinplan={
-      plan_name:this.state.plan_name,
-      notes:this.state.notes,
+
+    const newskinplan = {
+      plan_name: this.state.plan_name,
+      notes: this.state.notes,
     };
-    
+
     fetch(apiUrl, {
       method: 'POST',
       body: JSON.stringify(newskinplan),
@@ -111,36 +109,88 @@ let local = false;
     console.log('END');
   }
 
- 
+
+
+
+  /* //addprod = (e) => {
+
+    const  apiUrl= 'http://localhost:58031/api/Cos/AddProdToPlan/${this.state.appUser_id}';
+
+    console.clear();
+    e.preventDefault()
+
+    const prodtoplan = {
+      prod_id: this.state.prod_id,
+      plan_id: this.state.plan_id,
+    };
+    fetch(apiUrl, {
+      method: 'POST',
+      body: JSON.stringify(prodtoplan),
+      headers: new Headers({
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Accept': 'application/json; charset=UTF-8',
+      })
+
+        .then(res => {
+          console.log('res=', res);
+          console.log('res.status', res.status);
+          console.log('res.ok', res.ok);
+          return res.json()
+        })
+        .then(
+          (result) => {
+            console.log("fetch POST= ", result);
+            console.log(result.prod_id);
+            console.log(this.state);
+          },
+          (error) => {
+            console.log("err post=", error);
+          });
+  
+      console.log('END');
+    }
+  }
+
+  filterproducts = (e) => {
+    // filter products by type from list 
+  }
+*/
+
   render() {
 
     return (
 
       <div>
-      
-        <h4 style={{marginTop:35}}>יצירת תוכנית טיפוח </h4>
 
-        <img alt="prod" height="150" width="150" src={require("../assets/images/skinplan.png")}/> 
-        <h4 style={{marginTop:30}}>יצירת תוכנית טיפוח </h4>
-        <h3 style={{color:"#c4a092",fontSize:15,textAlign:'center'}} > פרטי משתמש </h3> 
+        <h4 style={{ marginTop: 35 }}>יצירת תוכנית טיפוח </h4>
 
+        <img alt="prod" height="150" width="150" src={require("../assets/images/skinplan.png")} />
+
+
+        <div style={{ border: '1px solid black' }}>
+          <h3 style={{ color: "#c4a092", fontSize: 15, textAlign: 'center' }} > פרטי משתמש </h3>
+          <div >
+            {this.state.users.map((users) => <UserInfo key={users.appUser_id} users={users.appUser_id} />)}
+          </div>
+        </div>
+
+        <div style={{ border: '1px solid black' }}>
+          <h3 style={{ color: "#c4a092", fontSize: 15, textAlign: 'center' }} > פרטי התוכנית </h3>
+          <InputUser value={this.plan_name} name="plan_name" type="text" label=" שם התוכנית  " placeholder="שם התוכנית   " onChange={(e) => { this.setState({ plan_name: e.target.value }) }} />
+          <InputUser value={this.notes} name="notes" type="text" label="  הערות   " placeholder="הערות   " onChange={(e) => { this.setState({ notes: e.target.value }) }} /><br />
+          <ButtonLogIn style={{ backgroundColor: "#c4a092", color: "white", fontSize: 15, width: '60%', height: 30, borderColor: "#e8e8e8", borderWidth: 1, borderRadius: 50, margin: 10 }} name="שמירת פרטי תוכנית " onClick={this.addSkinPlan} />
+        </div>
+
+
+        <h3 style={{ margin: 30, color: "#c4a092", fontSize: 15, textAlign: 'center' }} >הוספת מוצרים </h3>
         <div >
-               {this.state.users.map((users) => <UserCard key={users.appUser_id} users={users}/>)}
-            </div> 
+          {this.state.products.map((products) => <CardAddProdToPlan key={products.prod_id} products={products} />)}
+        </div>
 
-            <h3 style={{color:"#c4a092",fontSize:15,textAlign:'center'}} > פרטי התוכנית </h3>
-        <InputUser value={this.plan_name} name="plan_name" type="text" label=" שם התוכנית  " placeholder="שם התוכנית   " onChange={(e)=>{this.setState({plan_name:e.target.value})}}/>
-        <InputUser value={this.notes} name="notes" type="text" label="  הערות   " placeholder="הערות   " onChange={(e)=>{this.setState({notes:e.target.value})}}/><br/>
-        
-        <h3 style={{color:"#c4a092",fontSize:15,textAlign:'center'}} >הוספת מוצרים </h3>
-          <div > 
-           {this.state.products.map((products) => <CardAddProdToPlan  key={products.prod_id} products={products}/>)} 
-         </div>
-       
-         <h3 style={{color:"#c4a092",fontSize:15,textAlign:'center'}} > מוצרים שנוספו לתוכנית </h3>
+          <h3 style={{ color: "#c4a092", fontSize: 15, textAlign: 'center' }} > מוצרים שנוספו לתוכנית </h3>
+          <ButtonLogIn style={{ margin: 30, backgroundColor: "black", color: "white", fontSize: 15, width: '80%', height: 40, borderColor: "#e8e8e8", borderWidth: 1, borderRadius: 50 }} name="שמירה והוספת מוצרים " />
 
-        <ButtonLogIn  style={{margin:30,backgroundColor:"#c4a092",color:"white",fontSize:15,width:'80%',height:40,borderColor:"#e8e8e8" , borderWidth:1,borderRadius:50}} name="שמירה " onClick={this.addSkinPlan}/>
-        {this.state.showPopup ? 
+        {this.state.showPopup ?
           <PopUpCos
             header=' תוכנית טיפוח נוספה בהצלחה '
             text=' נבדוק את מספר העסק שלך ותוכלי להתחיל לטפל בלקוחות בעוד כ24 שעות'
@@ -148,6 +198,7 @@ let local = false;
           />
           : null
         }
+        
       </div>
     )
   }
